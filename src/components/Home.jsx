0606ca281e396dbae91d1ac1048878cb66c6107e
GJ Pineda <121123012+gjjrpin@users.noreply.github.com>
2024-12-11 23:30:14 -0500

    modified skills and home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,37 +2,38 @@ import React, { useState } from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
 import Hero from "../assets/hero.png";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logooutline from "../assets/logo-outline.svg";
 
 const Home = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
   return (
-    <div name="home" className="w-full h-screen mb-16">
+    <div name="home" className="w-full min-h-screen">
       {/* Container */}
-      <div className="h-[750px] flex">
-        <div className="flex flex-col justify-center h-full">
-          <div className="">
-            <span className="text-4xl sm:text-3xl text-[#ffffff]">
+      <div className="min-h-screen h-full justify-center flex flex-col md:flex-row gap-y-6">
+        <div className="flex flex-col justify-center">
+          <div>
+            <span className="text-4xl sm:text-3xl text-gray-300">
               I am a&nbsp;
             </span>
             <span className="text-4xl sm:text-3xl text-[#c470db]">
               Fraud Analyst&nbsp;
             </span>
-            <span className="text-4xl sm:text-3xl text-[#ffffff]">
+            <span className="text-4xl sm:text-3xl text-gray-300">
               and&nbsp;
             </span>
             <span className="text-4xl sm:text-3xl text-[#c470db]">
               Cyber Security enthusiast
             </span>
           </div>
-          <p className="text-[#FFFFFF] py-4 max-w-[700px] leading-loose">
-            I prevent and investigate fraud, enhancing client trust through
-            problem-solving and communication.
+          <p className="text-gray-300 py-4 max-w-[700px] leading-loose">
+            I prevent and investigate fraud.
           </p>
           <div>
             <Link onClick={handleClick} to="work" smooth={true} duration={500}>
-              <button className="text-white group border-2 px-6 py-3 my-2 flex items-center border-[#c470db] hover:bg-[rgba(196,112,219,0.15)] hover:border-[#c470db]">
+              <button className="text-gray-300 group border-2 px-6 py-3 my-2 flex items-center border-[#c470db] hover:bg-[rgba(196,112,219,0.15)] hover:border-[#c470db]">
                 View Work
                 <span className="group-hover:rotate-90 duration-300">
                   <HiArrowNarrowRight className="ml-3" />
@@ -41,12 +42,25 @@ const Home = () => {
             </Link>
           </div>
         </div>
-        <div className="hidden w-7/12 lg:flex  items-center">
+        <div className="relative md:w-7/12 lg:flex flex-col justify-center">
           <img
-            className="scale-110 border-b-2 border-[#c470db]"
-            src={Hero}
-            alt="Hero image"
+            className="absolute top-16 left-4 md:top-64 md:left-0 h-16 w-16 md:h-24 md:w-24"
+            src={Logooutline}
           />
+          <img
+            className="border-b-2 border-[#c470db] h-full w-full md:h-auto md:w-auto"
+            src={Hero}
+            alt="Hero"
+          />
+          <div className="border flex items-center mx-12 md:mx-22 gap-4 py-2 px-4">
+            <a
+              className="text-gray-300"
+              href="https://drive.google.com/file/d/1zaYvf5n7hoyBsOVlD_SZx4syfgKQLKnE/view?usp=share_link"
+            >
+              <BsFillPersonLinesFill size={30} />
+            </a>
+            <p className="text-gray-300">&lt;&lt;&lt; Click for resume</p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,59 +1,47 @@
 import React from "react";
-
-import AWS from "../assets/aws.png";
-import SASS from "../assets/sass.png";
-import JavaScript from "../assets/javascript.png";
-import ReactImg from "../assets/react.png";
-import Node from "../assets/node.png";
-import Heroku from "../assets/heroku.png";
-import Tailwind from "../assets/tailwind.png";
-import Mysql from "../assets/mysql.png";
+import Logooutline from "../assets/logo-outline.svg";
 import Hashtag from "./Hashtag";
 
 const Skills = () => {
   return (
     <div name="skills" className="w-full h-screen text-gray-300">
       {/* Container */}
-      <div className="py-4 flex flex-col justify-center w-full h-full">
+      <div className="pt-52 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline-flex items-center">
             <Hashtag />
-            skills<span class="block w-56 h-[2px] bg-[#c470db] mx-2"></span>
+            skills<span class="block w-32 h-[2px] bg-[#c470db] mx-2"></span>
           </p>
         </div>
-
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
-            <p className="my-4">JAVASCRIPT</p>
+        <div className="mt-16 flex flex-col md:flex-row">
+          <div className="relative md:w-2/5 h-64">
+            <img
+              className="absolute top-16 right-16"
+              src={Logooutline}
+              alt="logo"
+            />
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
-            <p className="my-4">REACT</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={SASS} alt="HTML icon" />
-            <p className="my-4">SASS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
-            <p className="my-4">TAILWIND</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Node} alt="HTML icon" />
-            <p className="my-4">NODE JS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Mysql} alt="HTML icon" />
-            <p className="my-4">MYSQL</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Heroku} alt="HTML icon" />
-            <p className="my-4">HEROKU</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={AWS} alt="HTML icon" />
-            <p className="my-4">AWS</p>
+          <div className="md:w-1/2">
+            <div className="grid md:grid-cols-2 gap-4">
+              <div className="border">
+                <h3 className="border-b p-2 text-white">Languages</h3>
+                <p className="p-2">Javascript</p>
+                <p className="p-2">Typescript</p>
+                <p className="p-2">Python</p>
+              </div>
+              <div className="border">
+                <h3 className="border-b p-2 text-white">Database</h3>
+                <p className="p-2">MySQL</p>
+                <p className="p-2">MySQL</p>
+                <p className="p-2">MySQL</p>
+              </div>
+              <div className="border">
+                <h3 className="border-b p-2 text-white">Fraud</h3>
+                <p className="p-2">Cybersecurity</p>
+                <p className="p-2">Cybersecurity</p>
+                <p className="p-2">Cybersecurity</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
